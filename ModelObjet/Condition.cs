1b68fc905700d3dc69fc0035c68a4860b79796d5
joni75014 <45243676+joni75014@users.noreply.github.com>
2019-09-20 08:18:35 +0200

    Ajoutez des fichiers projet.

---
+++ ModelObjet/Condition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ModelObjet
+{
+    public class Condition
+    {
+        const int nbJours = 30;
+        // Permet de savoir si on a le droit d'être remboursé
+        // On l'est à condition que le nombre de jours ne dépasse pas 30 !
+        public static bool Valider(int unNbDeJours)
+        {
+            return true;
+        }
+        // Permet de renvoyer le montant MAX remboursé en fonction de la catégorie
+        public static int CalculerMontantMax(string uneCategorie)
+        {
+            return 0;
+        }
+        // Permet de retourner le total remboursé en fonction de tous les critères
+        public static double CalculerMontantRembourse(int unNbDeJours, string uneCategorie, bool estMembre, string unEtat, int unPrix)
+        {
+            return 0;
+
+        }
+        // Permet de renvoyer la réduction si on est membre ou pas
+        public static double CalculerReductionMembre(bool estMembre)
+        {
+            return 0;
+        }
+        // Permet de renvoyer la réduction en fonction de l'état de l'achat
+        public static double CalculerReduction(string unEtat)
+        {
+            return 0;
+        }
+    }
+}
---
+++ ProjetDeTests/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelObjet;
+
+namespace ProjetDeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void ValiderTest()
+        {
+            // Le nombre de jours d'achat est < à 30 jours
+            
+
+            // Le nombre de jours d'achat est > à 30 jours
+            
+        }
+
+        [TestMethod()]
+        public void CalculerMontantMaxTest()
+        {
+            // Pour la catégorie livre
+            
+
+            // Pour la catégorie jouet
+            
+
+            // Pour la catégorie informatique
+            
+        }
+
+        [TestMethod()]
+        public void CalculerMontantRembourseTest()
+        {
+            // Un livre achété 24 euros depuis 15 jours avec un état "Très abimé" en étant non membre
+
+            // Un livre achété 24 euros depuis 15 jours avec un état "Bon" en étant membre
+
+        }
+
+        [TestMethod()]
+        public void CalculerReductionMembreTest()
+        {
+            // Il est membre
+            
+
+
+            // Il n'est pas membre
+            
+
+        }
+
+        [TestMethod()]
+        public void CalculerReductionTest()
+        {
+            // Pour un état "bon"
+            
+
+
+            // Pour un état "abimé"
+            
+
+        }
+    }
+}
