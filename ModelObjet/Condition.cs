1cb6a9443ce5fa6db490a814a1ea83009d247b92
joni75014 <45243676+joni75014@users.noreply.github.com>
2019-09-20 10:10:33 +0200

    Modif Test

--- ModelObjet/Condition.cs
+++ ModelObjet/Condition.cs
@@ -9,28 +9,81 @@ namespace ModelObjet
         // On l'est à condition que le nombre de jours ne dépasse pas 30 !
         public static bool Valider(int unNbDeJours)
         {
-            return true;
+            
+            if (unNbDeJours < nbJours) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         // Permet de renvoyer le montant MAX remboursé en fonction de la catégorie
         public static int CalculerMontantMax(string uneCategorie)
         {
-            return 0;
+            
+            int montantMax = 0;
+            if(uneCategorie == "Livres")
+            {           
+                montantMax = 30;
+            }
+
+            if(uneCategorie == "Jouets")
+            {             
+                montantMax = 50;
+            }
+            if(uneCategorie == "Informatique")
+            {
+                montantMax = 1000;
+            }
+
+            return montantMax;
         }
         // Permet de retourner le total remboursé en fonction de tous les critères
         public static double CalculerMontantRembourse(int unNbDeJours, string uneCategorie, bool estMembre, string unEtat, int unPrix)
         {
+            
+
             return 0;
 
         }
         // Permet de renvoyer la réduction si on est membre ou pas
         public static double CalculerReductionMembre(bool estMembre)
         {
-            return 0;
+           double reductionMembre = 0;
+            int prix = 0;
+            if(estMembre == true)
+            {
+                reductionMembre = 0;
+            }
+            else
+            {
+                reductionMembre = 0.2 * prix;
+            }
+            
+            return reductionMembre;
         }
         // Permet de renvoyer la réduction en fonction de l'état de l'achat
         public static double CalculerReduction(string unEtat)
         {
-            return 0;
+           double reduction = 0;
+          
+            if (unEtat == "Abimé")
+            {
+                reduction = 0.3;
+            }
+            if (unEtat == "Très abimé")
+            {
+                reduction = 0.3;
+            }
+            else
+            {
+                reduction = 0.1;
+            }
+
+            return reduction; 
         }
+
     }
 }
+
--- ProjetDeTests/UnitTest1.cs
+++ ProjetDeTests/UnitTest1.cs
@@ -11,23 +11,28 @@ namespace ProjetDeTests
         public void ValiderTest()
         {
             // Le nombre de jours d'achat est < à 30 jours
-            
+            bool jour1 = Condition.Valider(29);
+            Assert.AreEqual(true, jour1);
 
             // Le nombre de jours d'achat est > à 30 jours
-            
+            bool jour2 = Condition.Valider(45);
+            Assert.AreEqual(false, jour2);
         }
 
         [TestMethod()]
         public void CalculerMontantMaxTest()
         {
             // Pour la catégorie livre
-            
-
+            string categorie1 = Condition.CalculerMontantMax("Livres").ToString();
+            Assert.AreEqual(30, categorie1);
             // Pour la catégorie jouet
-            
+            string categorie2 = Condition.CalculerMontantMax("Jouets").ToString();
+            Assert.AreEqual(30, categorie2);
 
             // Pour la catégorie informatique
-            
+            string categorie3 = Condition.CalculerMontantMax("Informatique").ToString();
+            Assert.AreEqual(30, categorie3);
+
         }
 
         [TestMethod()]
@@ -43,11 +48,15 @@ namespace ProjetDeTests
         public void CalculerReductionMembreTest()
         {
             // Il est membre
-            
+            double membre1 = Condition.CalculerReductionMembre(true);
+            Assert.AreEqual(0.80, membre1);
+
+
 
 
             // Il n'est pas membre
-            
+            double membre2 = Condition.CalculerReductionMembre(false);
+            Assert.AreEqual()
 
         }
 
@@ -55,12 +64,13 @@ namespace ProjetDeTests
         public void CalculerReductionTest()
         {
             // Pour un état "bon"
-            
-
+            double reduction1 = Condition.CalculerReduction("bon");
+            Assert.AreEqual(0.1, reduction1);
 
             // Pour un état "abimé"
-            
 
+            double reduction2 = Condition.CalculerReduction("Abimé");
+            Assert.AreEqual(0.3, reduction2);
         }
     }
 }
